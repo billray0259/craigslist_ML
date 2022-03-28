08267ee65e4cbfed54b92f51a3bff201707fef05
William Ray <bill.ray0259@gmail.com>
2022-03-27 22:00:24 -0400

    first commit

---
+++ lib.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import os
+from PIL import Image
+
+def get_search_filename(search_type, limit):
+    filename = f"{search_type}_{limit}_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.json"
+    return filename
+
+def get_texts(search):
+    titles = []
+    for post_id, post in search.items():
+        titles.append(post["name"])
+    return titles
+
+def get_ids(search):
+    ids = []
+    for post_id in search:
+        ids.append(post_id)
+    return ids
+
+def get_images(search, image_dir):
+    images = []
+    for post_id in search:
+        filename = os.path.join(image_dir, post_id + ".jpg")
+        if os.path.exists(filename):
+            # read image with PIL
+            image = Image.open(filename)
+            images.append(image)
+    return images
